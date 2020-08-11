e80f5a158bae6066435df0ee6c48afed5976aeeb
yusluv1234 <yusluv124@gmail.com>
2020-08-11 15:55:23 +0100

    Updates

---
+++ precache-manifest.3ff5a8473469598bb9687ec3b71088e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8034ae83ee8120e9e4d658289fe48d6",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "62c30bc1b770fed6e3db",
+    "url": "/Monsters-Rolodex/static/css/main.e6a350c3.chunk.css"
+  },
+  {
+    "revision": "341c206e3a9fefb1039f",
+    "url": "/Monsters-Rolodex/static/js/2.f486444a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.f486444a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c30bc1b770fed6e3db",
+    "url": "/Monsters-Rolodex/static/js/main.1471df59.chunk.js"
+  },
+  {
+    "revision": "2624f4b6b9fed47b6233",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.920eb9b7.js"
+  }
+]);
